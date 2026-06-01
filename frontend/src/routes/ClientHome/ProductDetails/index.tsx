bdee6b26353dc4d3ed9ee5946d4632b017e7839f
Ryan Souza <ryanssebadelhe@gmail.com>
2026-06-01 17:42:56 -0300

    First test request with axios

--- frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,16 +1,28 @@
 import "./styles.css";
 
-import ButtonPrimary from "../../../components/ButtonPrimary";
 import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 
-import * as productService from "../../../services/product-service";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import type { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+
+    axios.get(`http://localhost:8080/products/${params.productId}`).then((response) => {
+      console.log(response);
+      setProduct(response.data);
+    });
+
+  }, []);
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
