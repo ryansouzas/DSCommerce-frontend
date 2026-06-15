c0b2e96c1f69cfbd15d05f9859e93bf530bd305d
Ryan Souza <ryanssebadelhe@gmail.com>
2026-06-15 10:35:34 -0300

    Cart key

--- frontend/src/localstorage/cart-repository.ts
+++ frontend/src/localstorage/cart-repository.ts
@@ -1,12 +1,13 @@
 import type { OrderDTO } from "../models/order";
+import { CART_KEY } from "../utils/system";
 
 export function save(cart : OrderDTO){
     const str = JSON.stringify(cart);
 
-    localStorage.setItem("com.devsuperior/Cart", str)
+    localStorage.setItem(CART_KEY, str)
 }
 
 export function get() : OrderDTO{
-    const str = localStorage.getItem("com.devsuperior/Cart") || '{"items": []}';
+    const str = localStorage.getItem(CART_KEY) || '{"items": []}';
     return JSON.parse(str);
 }
